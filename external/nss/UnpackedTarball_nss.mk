384799378c5b76cc72069cfd4d8b536883eac8ee
Jan-Marek Glogowski <glogow@fbihome.de>
2020-07-18 03:56:14 +0200

    nss: fix Windows Arm64 build
    Change-Id: I59834daebd6c9ccd1255be6f08e5f61b20ee06e4

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -26,6 +26,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
     external/nss/nss.bzmozilla1238154.patch \
     external/nss/macos-dlopen.patch.0 \
 	external/nss/nss.getopt.patch.0 \
+	external/nss/nss-win-arm64.patch \
     $(if $(filter iOS,$(OS)), \
         external/nss/nss-ios.patch) \
     $(if $(filter ANDROID,$(OS)), \
